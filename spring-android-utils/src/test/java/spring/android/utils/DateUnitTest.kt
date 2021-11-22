a16cff9589b4a5af2545fe925f4ad4e843ebfe25
chiclaim <chiclaim@gmail.com>
2021-11-22 15:08:01 +0800

    [add] 模拟SimpleDateFormat多线程问题

--- spring-android-utils/src/test/java/spring/android/utils/DateUnitTest.kt
+++ spring-android-utils/src/test/java/spring/android/utils/DateUnitTest.kt
@@ -6,6 +6,20 @@ import org.junit.Test
 class DateUnitTest {
 
     @Test
+    fun test(){
+        val sdf = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+        // it will throw : java.lang.NumberFormatException: multiple points
+        for (i in 0..10){
+            Thread{
+                sdf.parse("2021-11-22 12:23:18").also {
+                    println(it)
+                }
+            }.start()
+        }
+    }
+
+    @Test
     fun format(){
         Assert.assertEquals(DateUtil.format("yyyy-MM", System.currentTimeMillis()), "2021-09")
     }
